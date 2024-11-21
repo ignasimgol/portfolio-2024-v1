8cb6aa8cb84b586abb9bd89498a52821354041f7
[Ignasi Muñoz Gol] <ignasimgol@gmail.com>
2024-11-21 16:08:12 +0400

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,16 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import AboutMe from './pages/AboutMe';
+import Contact from './pages/Contact';
+import Navbar from './components/Navbar';
 import './index.css'; // Importa el archivo de estilos
 
 const App = () => {
   return (
     <Router>
       <div className="app">
-        {/* Barra de navegación simple */}
-        <nav className="navbar">
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-          </ul>
-        </nav>
-
-        {/* Configuración de las rutas */}
+       
+      <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<AboutMe />} />
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+// components/Navbar.jsx
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './css/Navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <ul className="navbar-list">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
+
