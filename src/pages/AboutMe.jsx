a0d6441ae3d653f1b8500acf23100b43e7e675c7
Ignasi <ignasimgol@gmail.com>
2025-01-28 13:59:02 +0400

    uae

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -50,7 +50,7 @@ const AboutMe = () => {
 
         <div className="card-text">
           <p>Hey! I'm Ignasi Muñoz. Welcome to my portfolio. I hope you enjoy my projects. Contact me if you have any question or if you want to work together!</p>
-          <p>With over a decade of dedicated coaching experience in basketball 🏀, I've embraced challenges, fostered teamwork, and honed invaluable skills such as responsibility, attention to detail, and active listening. My journey has taken me from courtsides in various countries, including Finland 🇫🇮, where I seamlessly adapted to diverse environments and collaborated with individuals from all walks of life.</p>
+          <p>With over a decade of dedicated coaching experience in basketball 🏀, I've embraced challenges, fostered teamwork, and honed invaluable skills such as responsibility, attention to detail, and active listening. My journey has taken me from courtsides in various countries, including Finland 🇫🇮 and now the UAE 🇦🇪, where I seamlessly adapted to diverse environments and collaborated with individuals from all walks of life.</p>
           <p>Driven by an insatiable curiosity and an unyielding desire to learn, I've ventured into the world of programming. Currently pursuing a Master's in <strong>Front-End Development</strong> at Universitat Oberta de Catalunya, I am channeling my passion for creativity and innovation into mastering the intricacies of web development.</p>
           <p>As an individual with an innate talent for visual expression and a keen interest in the dynamic realm of <strong>3D design</strong>, I am further enhancing my skills through a course in Three.js, paving the way for groundbreaking digital creations.</p>
         </div>
